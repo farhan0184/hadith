bed08eb645c0a1355224be9ee3f3294cea7136d5
farhan0184 <shahariar0184@gmail.com>
2024-04-30 21:39:51 +0600

    clean code

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,18 @@
 'use client'
-// import RightSide from "@/components/home/RightSide";
-// import TabSection from "@/components/home/TabSection";
-// import { getApiAction } from "@/redux/action/apiAction";
+
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
+
 
 
 export default function Home() {
   const router = useRouter()
-  // const dispatch = useDispatch()
-  // const { data } = useSelector((state) => state.api)
-  
-  // useEffect(() => {
-  //   dispatch(getApiAction())
-  // }, [dispatch])
 
   useEffect(() => {
      router.push('/bukhara/1')
   }, [])
   
   return (
-    <div className="flex gap-5 p-5 h-full">
-      {/* <TabSection />
-      <RightSide /> */}
-      {/* <p>Home</p> */}
-    </div>
+    <></>
   );
 }
